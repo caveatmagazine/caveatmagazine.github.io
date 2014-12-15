2e7cadc06b091930daeff8b2e5821947c8c3fdc2
Kyle Wilson <bluyam@tamu.edu>
2014-12-14 19:06:49 -0600

    moved caveat stuff to new repo

---
+++ style.js
@@ -0,0 +1,10 @@
+var main = function() {
+	/*var intro = function () {
+		$('#sub').animate({ opacity: '-=1'},500);
+		$('#mainlogo').animate({ width: '-=650px'},2000);
+	}
+	$('#sub').animate({ opacity: '+=1'},1000);
+	setTimeout(intro, 2000);*/
+};
+
+$(document).ready(main);
