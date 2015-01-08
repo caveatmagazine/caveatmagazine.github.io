9a7b5d3961df641b6810b0bf12ba668a44b53fa3
bluyam <bluyam@tamu.edu>
2015-01-08 16:41:29 -0600

    animation for main page

--- style.js
+++ style.js
@@ -1,10 +1,38 @@
 var main = function() {
-	/*var intro = function () {
-		$('#sub').animate({ opacity: '-=1'},500);
-		$('#mainlogo').animate({ width: '-=650px'},2000);
-	}
-	$('#sub').animate({ opacity: '+=1'},1000);
-	setTimeout(intro, 2000);*/
+    var hexa1 = function () { 
+        $(".hexes").animate({opacity:"1"},"slow");
+        setTimeout(hexa2,300);
+    }
+    var hexa2 = function () {
+        $(".hex2").animate({opacity:"1"}, "slow");
+        setTimeout(hexa3,300);
+    }
+    var hexa3 = function () {
+        $(".hex3").animate({opacity:"1"}, "slow");
+        setTimeout(hexa4,300);
+    }
+    var hexa4 = function () {
+        $(".hex4").animate({opacity:"1"}, "slow");
+        setTimeout(hexa5,300);
+    }
+    var hexa5 = function () {
+        $(".hex5").animate({opacity:"1"}, "slow");
+        setTimeout(hexa6,300);
+    }
+    var hexa6 = function () {
+        $(".hex6").animate({opacity:"1"}, "slow");
+        setTimeout(hexac,300);
+    }
+    var hexac = function () {
+        $(".hexc").animate({opacity:"1"}, "slow");
+        setTimeout(footer,300);
+    }
+    var footer = function () {
+        $("a").animate({opacity:"1"}, "slow");
+    }
+    $(".logo1").animate({opacity:"1"},"slow"); 
+    setTimeout(hexa1,300);
+
 };
 
 $(document).ready(main);
